10cb27d722ad900e942194c2f595abb11f1d7f98
Reza Nikravesh <reza.nikravesh80@gmail.com>
2022-08-04 04:50:28 +0430

    complete auth section

--- resources/views/includes/forms/captcha.blade.php
+++ resources/views/includes/forms/captcha.blade.php
@@ -1,12 +1,15 @@
 <div class="form-group">
-	<div class="captcha" style=margin-bottom:5px;>
-	{!! Captcha::img() !!} 
-	</div>
-	<input type="text" id="captcha" name="captcha" placeholder="Enter Captcha">
-	<button type="submit" style=background-color:#5865F2;border:none;>Go</button> 
-	@error('captcha')
-	<div class="error">
-		<small class="text-danger">{{ $errors->first('captcha') }}</small>
-	</div>
-	@enderror
+    <div class="captcha" style=margin-bottom:5px;>
+        {!! Captcha::img() !!}
+    </div>
+    <input type="text" id="captcha" name="captcha" placeholder="Enter Captcha">
+    @error('captcha')
+    <div class="error">
+        <small class="text-danger">{{ $errors->first('captcha') }}</small>
+    </div>
+    @enderror
+    <div class='btn-submit'>
+        <button type="submit" >Go</button>
+       
+    </div>
 </div>
